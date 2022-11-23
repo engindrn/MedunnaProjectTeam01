2f358aeb0a306fabdb1890ab7fa49b9018a4bf0f
Mehmet Akin <maebtab.858715@gmail.com>
2022-11-23 23:22:01 +0100

    M2

--- src/test/java/stepDefinitions/Ui/US004StepDef.java
+++ src/test/java/stepDefinitions/Ui/US004StepDef.java
@@ -5,6 +5,7 @@ import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import org.junit.Assert;
+import pages.US004Page;
 import utilities.ConfigReader;
 import utilities.Driver;
 import utilities.ReusableMethods;
